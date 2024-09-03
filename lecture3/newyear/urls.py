de57a1632842cd4570d814a856c1f2c5d91aa360
honggangyan <honggang.ds@gmail.com>
2024-09-03 23:22:17 +0200

    git push -u origin maingit push -u origin main

---
+++ lecture3/newyear/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+# A list of all urls that can be accessed to this particular hellop app
+# When "" is visited, the views.index function will be called.
+# Give a name to a path can be useful
+urlpatterns = [
+    path("", views.index, name="index")
+]
